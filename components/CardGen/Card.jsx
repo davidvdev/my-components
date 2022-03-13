e18c118c8d160945ebf75328d7376c361db04409
David Vogel <david@davidvdev.com>
2022-03-12 18:15:30 -0600

    card keys set up

--- components/CardGen/Card.jsx
+++ components/CardGen/Card.jsx
@@ -4,6 +4,17 @@ const Card = (props) => {
     const [isFlipped, setIsFlipped] = useState(true)
 
     const card = props.card
+    const faceKey = {
+        king: "K",
+        jack: "J",
+        queen: "Q"
+    }
+    const suitKey = {
+        hearts: "",
+        spades: "",
+        clubs: "",
+        diamonds: ""
+    }
 
     const cardFlip = (e) => {
         setTimeout(() => setIsFlipped(!isFlipped),100)
@@ -16,18 +27,26 @@ const Card = (props) => {
         thisCard.style.transitionDuration = "0.5s"
     }
 
+    const faceCard = () => (
+        <div className="innercard">
+            <h1>{faceKey[card.face]}</h1>
+            <h2>{card.suit}</h2>
+        </div>
+    )
+
+    const numCard = () => (
+        <div className='innerCard'>
+            <h2>{card.suit}</h2>
+            <h1>{card.cardValue}</h1>
+        </div>
+    )
+
     return(
         <div 
             className={isFlipped ? "flipped playing-card" : "playing-card"}
             onClick={(e) => cardFlip(e.currentTarget)}
         >
-            { card.face ? (
-                    <h1>{card.face}</h1>
-                ) : (
-                    <h1>{card.cardValue}</h1>
-                )
-            }
-            <h2>{card.suit}</h2>
+            {!isFlipped ? null : (card.face ? faceCard() : numCard())}
         </div>
     )
 }
